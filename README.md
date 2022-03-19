169251cac6900072864168e5fe188b2be370cbc3
Fernando Guedes <fernando.jobs@live.com>
2022-03-19 14:48:48 -0300

    Introdução aos Generics

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# generics-set-map
+<h1 align="center">Estudos sobre Generics, Set e Map.</h1>
+
+* Objetivo deste repositório:
+  - projeto destinado a estudos de Generics, Set e Map e exemplos básicos de aplicação;
+
+- Técnlogias utilizadas para o estudo
+    - Linguagem: JAVA
+    - Versão: 11;
+
+
+----------
+
+<h2>Generics</h2>
+<p>Generics permitem que classes, interface, e métodos possam ser parametrizados por tipo. Seus beneficios são:</p>
+<li>Reuso</li>
+<li>Type safety</li>
+<li>Performance</li>
+
+* Uso Comum: coleções
+~~~~java
+List<String> list = new ArrayList<>();
+list.add("Teste");
+String name = list.get(0);
+~~~~
---
+++ src/Program.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class Program {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        // Exemplo de implementação com o tipo Integer
+        PrintService<Integer> printServiceInteger = new PrintService<>();
+
+        System.out.println("How many values? ");
+        int n = scanner.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            Integer value = scanner.nextInt();
+            printServiceInteger.addValue(value);
+        }
+
+        printServiceInteger.print();
+        printServiceInteger.printFirst();
+
+        System.out.println("===========================================================");
+
+        // Exemplo de implementação com o tipo String
+        PrintService<String> printServiceString = new PrintService<>();
+
+        System.out.println("How many values? ");
+        n = scanner.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            String value = scanner.next();
+            printServiceString.addValue(value);
+        }
+
+        printServiceString.print();
+        printServiceString.printFirst();
+
+        scanner.close();
+    }
+}
