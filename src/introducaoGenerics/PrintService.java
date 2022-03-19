eff1c2d33358e1bef6a60c9330a59580dd809c42
Fernando Guedes <fernando.jobs@live.com>
2022-03-19 15:22:45 -0300

    Genéricos delimitados

--- src/introducaoGenerics/PrintService.java
+++ src/introducaoGenerics/PrintService.java
@@ -1,3 +1,5 @@
+package introducaoGenerics;
+
 import java.util.ArrayList;
 import java.util.List;
 
--- src/introducaoGenerics/Program.java
+++ src/introducaoGenerics/Program.java
@@ -1,3 +1,7 @@
+package introducaoGenerics;
+
+import introducaoGenerics.PrintService;
+
 import java.util.Scanner;
 
 public class Program {
